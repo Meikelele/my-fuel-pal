39c259ad695c9d66ce095af347970b1b8ff63db2
Michael_Bak <lenovo.bak2002@gmail.com>
2024-04-02 13:19:22 +0200

    Set up

---
+++ myfuelpal/src/main/java/com/example/myfuelpal/MyfuelpalApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfuelpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfuelpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfuelpalApplication.class, args);
+	}
+
+}
---
+++ myfuelpal/src/test/java/com/example/myfuelpal/MyfuelpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfuelpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfuelpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
