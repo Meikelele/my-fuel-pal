a637236cfbc79ed60363e998751b562979dc48e4
michalBak <lenovo.bak2002@gmail.com>
2024-06-08 22:19:04 +0200

    Token expiration extended

--- myfuelpal/src/main/java/com/example/myfuelpal/config/JwtService.java
+++ myfuelpal/src/main/java/com/example/myfuelpal/config/JwtService.java
@@ -36,7 +36,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 * 60))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
